71c235ef0a9b83dc140af90ac1ff132bf6fb5322
alenjb <alenjb@naver.com>
2025-06-16 00:00:51 +0900

    feat: swagger 추가
    - build.gradle에 swagger 의존성 추가
    - StoreDelaySummaryController에서 접속경로를 OrderController에서의 접속와 같이 통일(api/stores/ -> /stores/)

--- src/main/java/com/example/delivery/stat/controller/StoreDelaySummaryController.java
+++ src/main/java/com/example/delivery/stat/controller/StoreDelaySummaryController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/stores")
+@RequestMapping("/stores")
 @RequiredArgsConstructor
 public class StoreDelaySummaryController {
 
