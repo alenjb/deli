b2d8cf4f8237061847da9cc536da6ffed3f287c5
alenjb <alenjb@naver.com>
2025-06-17 22:05:17 +0900

    chore(review): 사용하지 않는 리뷰 디렉토리 및 관련 파일 삭제

--- src/main/java/com/example/delivery/review/repository/ReviewRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.delivery.review.repository;
-
-import com.example.delivery.review.domain.Review;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReviewRepository extends JpaRepository<Review, Long> {
-}
